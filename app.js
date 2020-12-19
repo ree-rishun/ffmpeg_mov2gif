9660bd70305eadfc8571dab2a17e643c5b895cf2
ree-rishun <ree0432@gmail.com>
2020-12-19 23:29:34 +0900

    デモに合わせてログの有効化

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const { createFFmpeg, fetchFile } = FFmpeg
-const ffmpeg = createFFmpeg({ log: false, progress: p => displayProgress(p)})
+const ffmpeg = createFFmpeg({ log: true, progress: p => displayProgress(p)})
 
 // エレメントの定義
 const imagePreview = document.getElementById('imagePreview')    // GIF変換結果
